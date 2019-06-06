a02eb90577d57d5a98e51ecc605367c71b74af86
Lucas <lcassimiro1@gmail.com>
2019-06-06 09:03:42 -0300

    Adicionado projeto de como utilizar um reCAPTCHA

---
+++ ReCaptchaProject/ReCaptchaProject/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ReCaptchaProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FormSubmit()
+        {
+            //Validate Google recpatcha
+            var response = Request["g-recaptcha-response"];
+            string secretKey = "Your Secret Key";
+            var client = new WebClient();
+            var result = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secretKey, response));
+            var obj = JObject.Parse(result);
+            var status = (bool)obj.SelectToken("success");
+            ViewBag.Message = status ? "Google ReCaptcha validation success" : "Google ReCaptcha validation failed";
+
+
+            return View("Index");
+        }
+    }
+}
